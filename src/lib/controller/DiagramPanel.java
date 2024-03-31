38bae5ce968cb2565cd37b10b9a6feec49a8087f
GR1SLy <nikonchyk@inbox.ru>
2024-03-31 13:14:05 +0700

    Added errors to diagram

--- src/lib/controller/DiagramPanel.java
+++ src/lib/controller/DiagramPanel.java
@@ -3,7 +3,6 @@ package lib.controller;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.JLabel;
@@ -12,15 +11,14 @@ import javax.swing.SwingConstants;
 
 public class DiagramPanel extends JPanel {
 
-    private final int WIDTH, HEIGHT;
+    private final int WIDTH, UP_HEIGHT, DOWN_HEIGHT, HEIGHT;
 
     private ArrayList<Statistics> _statList;
 
-    private int _upperBound, _columnWidth, _columnOffset;
-    private double _step;
+    private int _upperBound, _lowerBound, _columnWidth, _columnOffset;
+    private double _upStep, _downStep;
 
-    private JLabel _upperBoundLabel, _middleBoundLabel, _mainLabel;
-    private JPanel _boundLabelPanel;
+    private JLabel _mainLabel;
 
     private JPanel _graphicsPanel;
     
@@ -34,47 +32,68 @@ public class DiagramPanel extends JPanel {
         _mainLabel.setForeground(Color.WHITE);
         add(_mainLabel, BorderLayout.PAGE_START);
 
-        _upperBoundLabel = new JLabel("", SwingConstants.LEFT);
-        _upperBoundLabel.setForeground(Color.WHITE);
-        _upperBoundLabel.setVerticalAlignment(JLabel.NORTH);
-
-        _middleBoundLabel = new JLabel("", SwingConstants.LEFT);
-        _middleBoundLabel.setForeground(Color.WHITE);
-        _middleBoundLabel.setVerticalAlignment(JLabel.NORTH);
-
-        _boundLabelPanel = new JPanel();
-        _boundLabelPanel.setLayout(new GridLayout(2, 1));
-        _boundLabelPanel.setOpaque(false);
-        _boundLabelPanel.add(_upperBoundLabel);
-        _boundLabelPanel.add(_middleBoundLabel);
-
-        add(_boundLabelPanel, BorderLayout.EAST);
-
         _graphicsPanel = new JPanel() {
-            private int x = 20;
-            private static final Color RECT = new Color(240, 170, 110);
+            private static final Color SPEED_RECT = new Color(240, 170, 110);
+            private static final Color ERROR_RECT = new Color(240, 160, 160);
             private static final Color LINE = new Color(100, 100, 100);
             private JLabel count;
 
             @Override
             public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                int x = 20;
                 for (Statistics stat : _statList) {
-                    int i = stat.getSpeed();
-                    int columnHeight = (int)(i * _step);
-                    g.setColor(RECT);
-                    g.fillRect(x, DiagramPanel.this.HEIGHT - columnHeight, _columnWidth, columnHeight);
+                    int speed = stat.getSpeed();
+                    int errors = stat.getErrors();
+                    int columnHeight = (int)(speed * _upStep);
+                    g.setColor(SPEED_RECT);
+                    g.fillRect(x, DiagramPanel.this.UP_HEIGHT - columnHeight, _columnWidth, columnHeight);
 
-                    count = new JLabel("" + i, JLabel.CENTER);
+                    g.setColor(ERROR_RECT);
+                    g.fillRect(x, DiagramPanel.this.UP_HEIGHT, _columnWidth, (int)(errors * _downStep));
+
+                    count = new JLabel("" + speed, JLabel.CENTER);
                     count.setVerticalAlignment(JLabel.NORTH);
                     count.setForeground(Color.WHITE);
-                    count.setBounds(x, DiagramPanel.this.HEIGHT - columnHeight, _columnWidth, 30);
+                    count.setBounds(x, DiagramPanel.this.UP_HEIGHT - columnHeight, _columnWidth, 30);
                     add(count);
 
                     g.setColor(LINE);
-                    g.drawLine((x - _columnOffset - ((_columnWidth / 2) / 2)), DiagramPanel.this.HEIGHT / 2, (x + ((_columnWidth / 2) / 2)), DiagramPanel.this.HEIGHT / 2);
+                    g.drawLine((x - _columnOffset - ((_columnWidth / 2) / 2)), DiagramPanel.this.UP_HEIGHT / 2, (x + ((_columnWidth / 2) / 2)), DiagramPanel.this.UP_HEIGHT / 2);
                     g.drawLine((x - _columnOffset - ((_columnWidth / 2) / 2)), 0, (x + ((_columnWidth / 2) / 2)), 0);
+                    g.drawLine((x - _columnOffset - ((_columnWidth / 2) / 2)), DiagramPanel.this.HEIGHT, (x + ((_columnWidth / 2) / 2)), DiagramPanel.this.HEIGHT);
                     x += _columnWidth + _columnOffset;
                 }
+
+                g.drawLine(20, DiagramPanel.this.UP_HEIGHT, DiagramPanel.this.WIDTH + 10, DiagramPanel.this.UP_HEIGHT);
+                count = new JLabel("" + _upperBound, JLabel.RIGHT);
+                count.setVerticalAlignment(JLabel.NORTH);
+                count.setForeground(Color.WHITE);
+                count.setBounds(DiagramPanel.this.WIDTH, 0, 45, 30);
+                add(count);
+                count = new JLabel("" + (_upperBound / 2), JLabel.RIGHT);
+                count.setVerticalAlignment(JLabel.NORTH);
+                count.setForeground(Color.WHITE);
+                count.setBounds(DiagramPanel.this.WIDTH, DiagramPanel.this.UP_HEIGHT / 2, 45, 30);
+                add(count);
+
+                count = new JLabel("speed", JLabel.RIGHT);
+                count.setVerticalAlignment(JLabel.NORTH);
+                count.setForeground(Color.WHITE);
+                count.setBounds(DiagramPanel.this.WIDTH, DiagramPanel.this.UP_HEIGHT - 30, 50, 30);
+                add(count);
+                count = new JLabel("errors", JLabel.RIGHT);
+                count.setVerticalAlignment(JLabel.NORTH);
+                count.setForeground(Color.WHITE);
+                count.setBounds(DiagramPanel.this.WIDTH, DiagramPanel.this.UP_HEIGHT + 10, 50, 30);
+                add(count);
+                
+                count = new JLabel("" + _lowerBound, JLabel.RIGHT);
+                count.setVerticalAlignment(JLabel.EAST);
+                count.setForeground(Color.WHITE);
+                count.setBounds(DiagramPanel.this.WIDTH, DiagramPanel.this.HEIGHT - 30, 45, 30);
+                System.out.println("DOWN_HEIGHT: " + (DiagramPanel.this.HEIGHT));
+                add(count);
             }
         };
         _graphicsPanel.setBackground(panelColor);
@@ -85,41 +104,60 @@ public class DiagramPanel extends JPanel {
         super();
         _statList = statList;
         WIDTH = width - 50;
-        HEIGHT = height - 10;
+        UP_HEIGHT = height - 200;
+        DOWN_HEIGHT = height - UP_HEIGHT - 50;
+        HEIGHT = height - 50;
         setDiagram();
     }
     
     public void findParams() {
-        findUpperBound(findMaxValue());
+        _upperBound = findUpperBound(findMaxValue(true));
+        _lowerBound = findLowerBound(findMaxValue(false));
         findColumnBounds();
-        findStep();   
+        _upStep = findStep(UP_HEIGHT);
+        _downStep = findStep(DOWN_HEIGHT);
     }
 
-    private int findMaxValue() {
+    private int findMaxValue(boolean isSpeed) {
         int max = 0;
         for (Statistics stat : _statList) {
-            int i = stat.getSpeed();
+            int i;
+            if (isSpeed) i = stat.getSpeed();
+            else i = stat.getErrors();
             if (i > max) max = i;
         }
         System.out.println("Max value: " + max);
         return max;
     }
 
-    private void findUpperBound(int maxValue) {
-        if (maxValue % 100 == 0) _upperBound = maxValue;
-        else if ( maxValue % 100 <= 50) _upperBound = ((maxValue / 100) * 100) + 50;
-        else _upperBound = ((maxValue / 100) + 1) * 100;
-        System.out.println("upperBound: " + _upperBound);
+    private int findUpperBound(int maxValue) {
+        int bound;
+        if (maxValue % 100 == 0) bound = maxValue;
+        else if ( maxValue % 100 <= 50) bound = ((maxValue / 100) * 100) + 50;
+        else bound = ((maxValue / 100) + 1) * 100;
+        System.out.println("bound: " + bound);
+        return bound;
     }
 
-    private void findStep() {
-        _step = (double)HEIGHT / (double)_upperBound;
-        System.out.println("_step: " + _step);
+    private int findLowerBound(int maxValue) {
+        int bound;
+        if (maxValue % 10 == 0) bound = maxValue;
+        else if ( maxValue % 10 <= 5) bound = ((maxValue / 10) * 10) + 5;
+        else bound = ((maxValue / 10) + 1) * 10;
+        System.out.println("bound: " + bound);
+        return bound;
+    }
+
+    private double findStep(int height) {
+        double step;
+        if (height == UP_HEIGHT) step = (double)height / (double)_upperBound;
+        else step = (double)height / (double)_lowerBound;
+        System.out.println("Step: " + step + "\theight: " + height);
+        return step;
     }
 
     private void findColumnBounds() {
-        int width = WIDTH;
-        _columnWidth = width / _statList.size();
+        _columnWidth = WIDTH / _statList.size();
         _columnOffset = _columnWidth / 10;
         _columnWidth -= _columnOffset;
         System.out.println("_columnWidth: " + _columnWidth + " _columnOffset: " + _columnOffset);
@@ -127,8 +165,6 @@ public class DiagramPanel extends JPanel {
 
     public void setDiagram() {
         findParams();
-        _upperBoundLabel.setText("" + _upperBound + "  ");
-        _middleBoundLabel.setText("" + (_upperBound / 2));
         System.out.println("Diagram");
     }
 }
